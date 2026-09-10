e5c475834694ec1020e0e0111450ac300f99281f
Aria Persian <160272091+ariaPersian@users.noreply.github.com>
2026-09-10 15:18:07 +0330

    fix: backfill optional report fields for older result objects

--- src/Functions/ZZZZZZZZZZZ-ReportCompatibility.ps1
+++ src/Functions/ZZZZZZZZZZZ-ReportCompatibility.ps1
@@ -33,3 +33,41 @@ function ConvertTo-SitecCompactTable {
     }
     $h+'</tbody></table>'
 }
+
+$script:SitecTwoPageReportCore = ${function:New-SitecCustomerReport}
+
+function Add-SitecOptionalProperty {
+    param($Object,[Parameter(Mandatory)][string]$Name,$Value)
+    if ($null -eq $Object) { return }
+    if (-not $Object.PSObject.Properties[$Name]) {
+        $Object | Add-Member -NotePropertyName $Name -NotePropertyValue $Value
+    }
+}
+
+function New-SitecCustomerReport {
+    [CmdletBinding()]
+    param([Parameter(Mandatory)]$Run,[Parameter(Mandatory)][string]$RunPath,[Parameter(Mandatory)]$Context)
+
+    # Normalize old/partial result objects before the strict two-page renderer sees them.
+    $stress=$null
+    if ($Run.Benchmark -and $Run.Benchmark.PSObject.Properties['Stress']) { $stress=$Run.Benchmark.Stress }
+    if ($null -eq $stress -and $Run.Benchmark -and $Run.Benchmark.PSObject.Properties['BurnIn']) { $stress=$Run.Benchmark.BurnIn }
+    if ($stress) {
+        Add-SitecOptionalProperty $stress 'TimedOut' $false
+        Add-SitecOptionalProperty $stress 'ActualSeconds' $(if($stress.PSObject.Properties['DurationSeconds']){$stress.DurationSeconds}else{0})
+        Add-SitecOptionalProperty $stress 'Error' ''
+        if ($stress.PSObject.Properties['CpuStress'] -and $stress.CpuStress) {
+            Add-SitecOptionalProperty $stress.CpuStress 'DutyPercent' 0
+        }
+        if ($stress.PSObject.Properties['MemoryVerification'] -and $stress.MemoryVerification) {
+            Add-SitecOptionalProperty $stress.MemoryVerification 'AllocatedMB' 0
+        }
+    }
+    if ($Run.Security) {
+        Add-SitecOptionalProperty $Run.Security 'HardwareIdentitySha256' ''
+        Add-SitecOptionalProperty $Run.Security 'Sha256' ''
+        Add-SitecOptionalProperty $Run.Security 'Signed' $false
+    }
+
+    & $script:SitecTwoPageReportCore -Run $Run -RunPath $RunPath -Context $Context
+}
