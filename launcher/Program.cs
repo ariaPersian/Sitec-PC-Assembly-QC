2c8037cd171002b9a6f026868f3087e6c254a84b
Aria Persian <160272091+ariaPersian@users.noreply.github.com>
2026-09-12 08:47:50 +0330

    Streamline operator inputs and mirror Asset ID to tamper seal
    * Streamline operator inputs and remove second seal field
    
    * Auto-mirror Asset ID to tamper seal and remove redundant inputs
    
    * Bump launcher payload version to 3.5.0
    
    * Remove operator from customer certificate metadata
    
    * Add streamlined operator workflow smoke tests
    
    * Fix internal profile assertion in operator workflow test

--- launcher/Program.cs
+++ launcher/Program.cs
@@ -11,7 +11,7 @@ namespace SitecQC.Launcher;
 
 internal static class Program
 {
-    private const string AppVersion = "3.4.0";
+    private const string AppVersion = "3.5.0";
     private const string PayloadResource = "SitecQC.Payload.zip";
 
     [STAThread]
